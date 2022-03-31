6e6fc383af5c683f61590e8297320e0d1991c9d8
khaifazil <khairal.fazil@gmail.com>
2022-03-31 19:56:54 +0800

    completed activity

--- compoundIf.go
+++ compoundIf.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert your code here
+	var year int
+	fmt.Println("Enter the year: ")
+	fmt.Scanln(&year)
+
+	if (year%4 == 0 && year%100 != 0) || year%400 == 0 {
+		fmt.Println("It's a leapyear!")
+	} else {
+		fmt.Println("It's not a leapyear.")
+	}
 }
